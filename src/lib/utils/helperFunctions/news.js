d12d1310d8e436e6e8d9b440f025d54c3d95a4d7
fr1ffo <86752407+fr1ffo@users.noreply.github.com>
2023-09-07 12:19:43 -0700

    Update news.js

--- src/lib/utils/helperFunctions/news.js
+++ src/lib/utils/helperFunctions/news.js
@@ -3,8 +3,8 @@ import { get } from 'svelte/store';
 import {news} from '$lib/stores';
 import { dynasty } from '$lib/utils/leagueInfo';
 
-// const REDDIT_DYNASTY = 'https://www.reddit.com/r/DynastyFF/new.json';
-// const REDDIT_FANTASY = 'https://www.reddit.com/r/fantasyfootball/new.json';
+const REDDIT_DYNASTY = 'https://www.reddit.com/r/DynastyFF/new.json';
+const REDDIT_FANTASY = 'https://www.reddit.com/r/fantasyfootball/new.json';
 const SERVER_API = '/api/fetch_serverside_news';
 
 export const getNews = async (servFetch, bypass = false) => {
@@ -15,11 +15,11 @@ export const getNews = async (servFetch, bypass = false) => {
 	const newsSources = [
 		smartFetch(SERVER_API, {compress: true}), 
 	];
-	// if(dynasty) {
-	// 	newsSources.push(getFeed(REDDIT_DYNASTY, processReddit));
-	// } else {
-	// 	newsSources.push(getFeed(REDDIT_FANTASY, processReddit));
-	// }
+	if(dynasty) {
+		newsSources.push(getFeed(REDDIT_DYNASTY, processReddit));
+	} else {
+		newsSources.push(getFeed(REDDIT_FANTASY, processReddit));
+	}
 
 	const [serverRes, reddit] = await waitForAll(...newsSources).catch((err) => { console.error(err); });
 	const serverData = await serverRes.json().catch((err) => { console.error(err); });
@@ -41,49 +41,49 @@ const getFeed = async (feed, callback) => {
 	}
 }
 
-// const processReddit = (rawArticles) => {
-// 	const bannedAuthors = [
-// 		"AutoModerator",
-// 		"FFBot",
-// 		"Brookskbrothers",
-// 		"FTAKJ"
-// 	]
-// 	const bannedIcons = [
-// 		"",
-// 		"self",
-// 		"thumbnail",
-// 		"default",
-// 	]
-// 	let finalArticles = [];
-// 	const children = rawArticles.children;
-// 	for(const rawArticle of children) {
-// 		const data = rawArticle.data;
-// 		if(bannedAuthors.includes(data.author)) {
-// 			continue;
-// 		}
-// 		const ts = data.created_utc * 1000;
-// 		const d = new Date(ts);
-// 		const icon = !bannedIcons.includes(data.thumbnail) ? data.thumbnail : `newsIcons/${data.subreddit}.png`;
-// 		const date = stringDate(d);
-// 		let article = `<a href="${data.url}" class="body-link">${data.url}</a>`;
-// 		if(data.selftext_html) {
-// 			article = decodeHTML(data.selftext_html);
-// 		}
-// 		if(data.secure_media_embed?.content) {
-// 			decodeHTML(data.secure_media_embed.content)
-// 		 }
-// 		finalArticles.push({
-// 			title: data.title,
-// 			article,
-// 			link: `https://www.reddit.com${data.permalink}`,
-// 			author: `${data.subreddit_name_prefixed} - u/${data.author}`,
-// 			ts,
-// 			date,
-// 			icon,
-// 		});
-// 	}
-// 	return finalArticles;
-// }
+const processReddit = (rawArticles) => {
+	const bannedAuthors = [
+		"AutoModerator",
+		"FFBot",
+		"Brookskbrothers",
+		"FTAKJ"
+	]
+	const bannedIcons = [
+		"",
+		"self",
+		"thumbnail",
+		"default",
+	]
+	let finalArticles = [];
+	const children = rawArticles.children;
+	for(const rawArticle of children) {
+		const data = rawArticle.data;
+		if(bannedAuthors.includes(data.author)) {
+			continue;
+		}
+		const ts = data.created_utc * 1000;
+		const d = new Date(ts);
+		const icon = !bannedIcons.includes(data.thumbnail) ? data.thumbnail : `newsIcons/${data.subreddit}.png`;
+		const date = stringDate(d);
+		let article = `<a href="${data.url}" class="body-link">${data.url}</a>`;
+		if(data.selftext_html) {
+			article = decodeHTML(data.selftext_html);
+		}
+		if(data.secure_media_embed?.content) {
+			decodeHTML(data.secure_media_embed.content)
+		 }
+		finalArticles.push({
+			title: data.title,
+			article,
+			link: `https://www.reddit.com${data.permalink}`,
+			author: `${data.subreddit_name_prefixed} - u/${data.author}`,
+			ts,
+			date,
+			icon,
+		});
+	}
+	return finalArticles;
+}
 
 var htmlEntities = {
     nbsp: ' ',
